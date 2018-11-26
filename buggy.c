4538d6d07a208b5992615c0205ce316b7d52b883
Firelion07 <dustin.ritter86@gmail.com>
2018-11-25 20:11:30 -0600

    Squashed all the bugs!

--- buggy.c
+++ buggy.c
@@ -13,25 +13,25 @@
 struct dog
 {
 	int age;
-	char name = [10];
+	char name[10];  //changed to char name[10]
 };
 
 
 void main()
 {
-	struct (dog) thing1:
+	struct dog thing1; //removed parentheses from "dog", changed colon to semicolon
 	struct dog thing2;
 
-	thing1.age = [5];
+	thing1.age = 5;  //removed brackets from value of thing1.age
 	thing2.age = 13;
 	
-	puts{"What is your dogs name? "};
-	gets(thing1.names);
+	puts("What is your dogs name? "); //changed curly braces to parentheses
+	gets(thing1.name); //changed "thing1.names" to "thing1.name"
 
 	strcpy(thing2.name, "Buster");
 
-	printf("Name: %i\n",thing1.name );
-	printf("Age: %i\n",thing1.age):
-	printf("Thing2 Name: %s\n,thing2.name);
-	printf("Thing2 Age %i\n',thing2.age);
+	printf("Name: %s\n",thing1.name );  //changed %i to %s
+	printf("Age: %i\n",thing1.age);		//changed colon to semicolon
+	printf("Thing2 Name: %s\n",thing2.name); //added missing quotation mark
+	printf("Thing2 Age: %i\n",thing2.age); //added colon to text for consistency, changed single quotation to double quotation 
 }
