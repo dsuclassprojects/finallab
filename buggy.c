5eab2f76ad264e1955b592c40a4b849c1c627cf1
joshuaFrederico <joshuafrederico@gmail.com>
2019-04-15 06:36:52 -0600

    github said my version was identical to the original...so I'm trying again like I usually have to...

--- buggy.c
+++ buggy.c
@@ -3,16 +3,16 @@
 
 /* Assignment:
 1. fix bugs in the code
-2. add comments (I wasn't sure what comments you had in mind, so I just explained the changes I made...)
+2. add comments (I wasn't sure what comments you had in mind, so I just explained the changes I made, or explained the code if no changes were necessary...)
 3. create a new branch on git
 4. git push changed buggy.c
 5. create pull request with your name and changes you made
 
 */
 
-struct dog
+struct dog // create new struct called 'dog'
 {
-	int age;
+	int age; // initialize integer variable
 	char name[10]; // Changed from improper var declaration 'char name = [10];'
 };
 
@@ -20,15 +20,15 @@ struct dog
 int main() // Changed return type from 'void' to 'int'...for some reason my compiler has never liked 'void'...
 {
 	struct dog thing1; // Replaced ':' with ';', replaced '(dog)' with 'dog'
-	struct dog thing2;
+	struct dog thing2; // create instance of 'dog' struct called 'thing2'
 
 	thing1.age = 5; // Removed brackets from around property value
-	thing2.age = 13;
+	thing2.age = 13; // assign age property of thing2 to 13
 	
 	puts("What is your dogs name? "); // Replaced curly braces with parentheses
 	gets(thing1.name); // Replaced 'names' with 'name'
 
-	strcpy(thing2.name, "Buster");
+	strcpy(thing2.name, "Buster"); // Assign string "Buster" to name property of thing2 using strcpy()
 
 	printf("Name: %s\n",thing1.name ); // Replaced wrong data type 'i' with 's'
 	printf("Age: %i\n",thing1.age); // Changed : to ;
