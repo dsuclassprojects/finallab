04fa24531462699657f757a90cde26e36b0a7a9d
slayer1201 <factotum1201@gmail.com>
2019-04-21 22:50:39 -0500

    line 16 removed = line 22 removed () & changed : to ; line 25 removed the [] line 28 changed {} to () line 29 changed thing1.names to thing1.name line 33 changed %i to %s line 34 changed : to ; line 35 added " here \n",t line 36 replaced ' with "  here \n",t

--- buggy.c
+++ buggy.c
@@ -13,25 +13,25 @@
 struct dog
 {
 	int age;
-	char name = [10];
+	char name[10]; //removed =
 };
 
 
 void main()
 {
-	struct (dog) thing1:
+	struct dog thing1; //removed () & changed : to ;
 	struct dog thing2;
 
-	thing1.age = [5];
+	thing1.age = 5; //removed the []
 	thing2.age = 13;
 	
-	puts{"What is your dogs name? "};
-	gets(thing1.names);
+	puts("What is your dogs name? "); //changed {} to ()
+	gets(thing1.name); //changed thing1.names to thing1.name
 
 	strcpy(thing2.name, "Buster");
 
-	printf("Name: %i\n",thing1.name );
-	printf("Age: %i\n",thing1.age):
-	printf("Thing2 Name: %s\n,thing2.name);
-	printf("Thing2 Age %i\n',thing2.age);
+	printf("Name: %s\n",thing1.name ); //changed %i to %s
+	printf("Age: %i\n",thing1.age); //changed : to ;
+	printf("Thing2 Name: %s\n",thing2.name); //added " here \n",t
+	printf("Thing2 Age %i\n",thing2.age); //replaced ' with "  here \n",t
 }
