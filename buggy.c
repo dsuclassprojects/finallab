da6b664283aaaf8a264f70c63ae947b6aa03729c
mattjager44 <Matt.Hoadley-Jager@trojans.dsu.edu>
2019-12-09 20:52:09 -0600

    made changes to buggy.c

--- buggy.c
+++ buggy.c
@@ -7,31 +7,30 @@
 3. create a new branch on git
 4. git push changed buggy.c
 5. create pull request with your name and changes you made
-
 */
 
 struct dog
 {
 	int age;
-	char name = [10];
+	char name [10];
 };
 
 
 void main()
 {
-	struct (dog) thing1:
+	struct dog thing1;
 	struct dog thing2;
 
-	thing1.age = [5];
+	thing1.age = 5;
 	thing2.age = 13;
 	
-	puts{"What is your dogs name? "};
-	gets(thing1.names);
+	puts("What is your dogs name? ");
+	gets(thing1.name);
 
 	strcpy(thing2.name, "Buster");
 
-	printf("Name: %i\n",thing1.name );
-	printf("Age: %i\n",thing1.age):
-	printf("Thing2 Name: %s\n,thing2.name);
-	printf("Thing2 Age %i\n',thing2.age);
+	printf("Name: %i\n",thing1.name);
+	printf("Age: %i\n",thing1.age);
+	printf("Thing2 Name: %s\n",thing2.name);
+	printf("Thing2 Age: %i\n",thing2.age);
 }
